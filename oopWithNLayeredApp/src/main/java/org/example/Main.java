ba6052a64a2169868100ec7f6b10eaa122c7c6e8
Feyza Karanfil Erat <56940562+feyzaerat@users.noreply.github.com>
2023-10-27 05:23:17 +0300

    .

--- oopWithNLayeredApp/src/main/java/org/example/Main.java
+++ oopWithNLayeredApp/src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+
+import org.example.business.ProductManager;
+import org.example.core.logging.DatabaseLogger;
+import org.example.core.logging.FileLogger;
+import org.example.core.logging.Logger;
+import org.example.core.logging.MailLogger;
+import org.example.dataAccess.JdbcProductDao;
+import org.example.entities.Product;
+
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception{
+        //VIEW
+        Product product1 = new Product(1,"Iphone Xr", 10000);
+
+        Logger[] loggers = {new DatabaseLogger(), new FileLogger(), new MailLogger()};
+
+        ProductManager productManager = new ProductManager(new JdbcProductDao(), List.of(loggers));
+        productManager.add(product1);
+
     }
 }
